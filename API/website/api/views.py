c70841ad18ded21be9e999b1058afbdfaf7544af
bananapie228 <galimzhan19358@gmail.com>
2025-11-24 19:39:20 +0500

    feat: creating views

--- API/website/api/views.py
+++ API/website/api/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+
+from .serializer import PostSerializer
 
 
 # Create your views here.
+class PostCreateList(generics.ListCreateAPIView):
+    queryset = Post().objects.all()
+    serializer_class = PostSerializer
